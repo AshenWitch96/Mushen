2c567c49319d8e92bf536253c704c62093770ee2
AshenWitch96 <maltipatelp46@gmail.com>
2026-02-22 11:34:05 +0530

    bot

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const { Client, GatewayIntentBits } = require('discord.js')
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+})
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return
+
+  if (message.content.toLowerCase().includes('sd')) {
+    setTimeout(() => {
+      message.reply('Reply after 7 minutes ⏰')
+    }, 7 * 60 * 1000)
+  }
+})
+
+client.login(process.env.TOKEN)
